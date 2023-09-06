c85938758d49c8d67871bd3be99307948c7b2d6e
gerardo zata <gerardo.zatarain@gmail.com>
2023-09-06 15:31:42 -0600

    Creation new project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PrimeNgFlexPipesApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PrimeNgFlexPipesApp');
-  });
+ 
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
